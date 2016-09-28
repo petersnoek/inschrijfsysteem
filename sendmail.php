f349fdb7ccf0c6b67b20489e60b20c7108c8eb2c
Peter Snoek <petersnoek@davinci.nl>
2016-09-28 09:50:53 +0200

    added more instructions to get phpmailer working - need to login to gmail account with a browser first

--- sendmail.php
+++ sendmail.php
@@ -1,8 +1,15 @@
 <?php
+// TO INSTALL PHPMAILER
+// 1. open dosbox, enter your app folder, type composer require phpmailer/phpmailer <enter>
+// (will download lots of things to /vendor folder)
+// 2. in the dosbox, type composer require league/oauth2-client <enter>
+// (will download lots of things to /vendor folder)
+
 // TO MAKE THIS WORK WITH YOUR GMAIL ACCOUNT
-// 1. log into your gmail account using a webbrowser
-// 2. in your browser, navigate to https://www.google.com/settings/u/0/security/lesssecureapps and allow non-secure apps
-// 3. in your browser, navigate to https://accounts.google.com/b/0/DisplayUnlockCaptcha and confirm
+// 0. If you are logged in at gmail with your own account, please log out first
+// 1. log into gmail account "ictambw@gmail.com" (user=ictambw@gmail.com, password=Studentje1) 
+// 2. navigate to https://www.google.com/settings/u/0/security/lesssecureapps and allow non-secure apps
+// 3. navigate to https://accounts.google.com/b/0/DisplayUnlockCaptcha and confirm
 // 4. now try to run this page again
 
 /**
@@ -21,7 +28,7 @@ $mail->isSMTP();
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 2;
+$mail->SMTPDebug = 0;
 //Ask for HTML-friendly debug output
 $mail->Debugoutput = 'html';
 //Set the hostname of the mail server
